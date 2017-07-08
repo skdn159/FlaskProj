a4e2c55eba6b6900a5ebb5095980e0a82e7809ce
Song Kyung <skdn159@naver.com>
2017-07-08 18:17:39 +0900

    add login

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
 
 app = Flask(__name__)
 
@@ -6,10 +9,33 @@ app = Flask(__name__)
 def helloWorld():
     return "HelloWorld"
 
+@app.route("/main")
+def main():
+    return "Here is mainPage"
+
+@app.route("/login_form")
+def login_form():
+    return render_template("login_form.html")
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        if(request.form['username'] =="ku"
+            and request.form['password'] =="1234"):
+            session["logged_in"] = True
+            session["username"]= request.form['username']
+            return request.form['username']+" 님 환영합니다"
+        else:
+            return "로그인 정보가 맞지 않습니다."
+    else:
+        return '잘못된 접근'
+
+secret_key = "sampleKey"
+
+app.secret_key = "abc"
+
 if __name__ == "__main__":
     app.run(debug = True, host = '127.0.0.1', port = 5009)
 
 
-#while 1==True:
-#    a = 10
 
